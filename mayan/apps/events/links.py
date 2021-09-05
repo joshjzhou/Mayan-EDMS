5b82ceada5d21f71e23ee1bc77e45a7ea4e1c5cb
Josh Zhou <joshzhou745@gmail.com>
2021-09-04 21:36:46 -0400

    Added labels for other icons in navbar

--- mayan/apps/events/links.py
+++ mayan/apps/events/links.py
@@ -77,6 +77,6 @@ link_object_event_types_user_subcriptions_list = Link(
 )
 link_user_notifications_list = Link(
     badge_text=get_unread_notification_count,
-    icon=icon_user_notifications_list, text='',
+    icon=icon_user_notifications_list, text='Notifications',
     view='events:user_notifications_list'
 )
